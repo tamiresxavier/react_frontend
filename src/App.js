9673f7c47350f6ef31e0aff825d9d0f38adc44d8
Tamires Xavier de Sousa <201715020023@ad.ifpb.edu.br>
2022-11-01 21:00:39 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React from "react";
 import "./index.css";
 
-export default function App() {
-  return (
-    <div class="form-container">
-      <form class="register-form">
-        {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
-        <input
-          id="name"
-          class="form-field"
-          type="text"
-          placeholder="Name"
-          name="name"
-        />
-        <button class="form-field" type="submit">
-          Register
-        </button>
-      </form>
-    </div>
-  );
+export default class App extends React.Component {
+
+  state = {
+    name: "",
+    lastName:"",
+    office: "",
+  }
+
+  create =() => {
+    var name = String(this.state.name);
+    this.setState({name});
+    var lastName = String(this.state.lastName);
+    this.setState({lastName});
+    var office = String(this.state.office);
+    this.setState({office});
+  }
+
+  render(){
+    return (
+      <div class="form-container">
+        <form class="register-form">
+          {/* Uncomment the next line to show the success message */}
+          {/* <div class="success-message">Success! Thank you for registering</div> */}
+          <input type="text" 
+          value={this.state.name} onChange={(e) => {this.setState({name: e.target.value})}} />
+          <input type="text" 
+          value={this.state.lastName} onChange={(e) => {this.setState({lastName: e.target.value})}} />
+          <input type="text"
+           value={this.state.office} onChange={(e) => {this.setState({office: e.target.value})}} />
+         
+          <br />
+  
+          <button onClick={this.create}> Register </button>
+
+          <br/>
+          Nome: {this.state.name}
+          Last Name: {this.state.lastName}
+          Office: {this.state.office}        
+        </form>
+      </div>
+    )
+  }
 }
